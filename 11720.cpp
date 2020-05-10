d887fd08b54a3d9d44e3dd18c4ff264f71c84b2c
Choi-Ji-Yun <cjy5025@naver.com>
2020-05-10 20:30:01 +0900

    Sum

---
+++ 11720.cpp
@@ -0,0 +1,14 @@
+/*#include <iostream>
+using namespace std;
+int main() {
+	int n, sum=0;
+	char* str;
+	cin >> n;
+	str = new char[n];
+	cin >> str;
+	for (int i = 0; i < n; i++)
+		sum = sum + (str[i] - 48);
+	cout << sum << endl;
+	delete[] str;
+	return 0;
+}*/
