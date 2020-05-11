e32a097d70e8c26412da4140d17411518afa189c
Choi-Ji-Yun <cjy5025@naver.com>
2020-05-11 20:24:56 +0900

    String(3)

---
+++ 11721.cpp
@@ -0,0 +1,23 @@
+//열개씩 끊어 출력
+/*#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 0, t = 0;
+	char arr[100];
+	cin >> arr;
+	while (1)
+	{
+		for (int i= n; i < n+10; i++) {
+			if (!arr[i]) break;
+			cout << arr[i];
+			t++;
+		}
+		n = t;
+		cout << "\n";
+		if (!arr[n]) break;
+		else continue;
+
+	}
+	return 0;
+}*/
