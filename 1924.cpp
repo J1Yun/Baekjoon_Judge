e92d6b8f042c08aa5992968917ece5c420cee789
Choi-Ji-Yun <cjy5025@naver.com>
2020-05-17 21:25:14 +0900

    Guess Day

---
+++ 1924.cpp
@@ -0,0 +1,34 @@
+//요일계산
+/*#include <iostream>
+using namespace std;
+int main()
+{
+	int x, y, d = 0;
+	int day[12] = { 0,31,28,31,30,31,30,31,31,30,31,30 };
+	cin >> x >> y;
+	for (int i = 0; i < x; i++) d = d + day[i];
+	switch ((d + y) % 7) {
+	case 0:
+		cout << "SUN";
+		break;
+	case 1:
+		cout << "MON";
+		break;
+	case 2:
+		cout << "TUE";
+		break;
+	case 3:
+		cout << "WED";
+		break;
+	case 4:
+		cout << "THU";
+		break;
+	case 5:
+		cout << "FRI";
+		break;
+	case 6:
+		cout << "SAT";
+		break;
+	}
+	return 0;
+}*/
