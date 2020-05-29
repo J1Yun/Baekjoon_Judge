58858e0922be6861864f075f129a9e3b16e30728
Choi-Ji-Yun <64777557+Choi-Ji-Yun@users.noreply.github.com>
2020-05-29 20:58:44 +0900

    Max_Index

---
+++ 2562.cpp
@@ -0,0 +1,16 @@
+/*#include <iostream>
+using namespace std;
+int main()
+{
+	int n[9], max = 0, index;
+	for (int i = 0; i < 9; i++)
+		cin >> n[i];
+	for (int i = 0; i < 9; i++) {
+		if (n[i] > max) {
+			max = n[i];
+			index = i + 1;
+		}
+	}
+	cout << max <<'\n'<< index << endl;
+	return 0;
+}*/
