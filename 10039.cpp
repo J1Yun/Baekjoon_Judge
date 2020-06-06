ced0f9f916f0aabffef4f1a66c87d20b8e0cbd3b
Choi-Ji-Yun <64777557+Choi-Ji-Yun@users.noreply.github.com>
2020-06-06 17:07:55 +0900

    Score

---
+++ 10039.cpp
@@ -0,0 +1,14 @@
+/*#include<iostream>
+using namespace std;
+int main()
+{
+	int score[5], sum = 0;
+	for (int i = 0; i < 5; i++) {
+		cin >> score[i];
+		if (score[i] < 40)
+			score[i] = 40;
+	}
+	for (int i = 0; i < 5; i++)
+		sum = sum + score[i];
+	cout << sum / 5 << endl;
+}*/
