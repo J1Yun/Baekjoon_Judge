e48edec5ab9d9a1fcec9541c9d37638abd5aa260
Choi-Ji-Yun <64777557+Choi-Ji-Yun@users.noreply.github.com>
2020-06-10 15:10:58 +0900

    Star(11)

---
+++ 10992.cpp
@@ -0,0 +1,22 @@
+//º°Âï±â 8
+/*#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;;
+	for (int i = 1; i < n; i++) {
+		for (int j = i; j < n; j++)
+			cout << " ";
+		cout << "*";
+		if (i == 1) {
+			cout << "\n";
+			continue;
+		}
+		for (int j = 1; j <= (i - 1) * 2 - 1; j++)
+			cout << " ";
+		cout << "*"<<endl;
+	}
+	for (int i = 1; i <= n * 2 - 1; i++) cout << "*";
+	return 0;
+}*/
