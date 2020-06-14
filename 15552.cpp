7e4696ac4cf3199edaf417275798798e00c69659
Choi-Ji-Yun <64777557+Choi-Ji-Yun@users.noreply.github.com>
2020-06-14 16:55:27 +0900

    Fast_In_Out

---
+++ 15552.cpp
@@ -0,0 +1,15 @@
+// 빠른 입출력 (시간단축)
+/*#include<iostream>
+using namespace std;
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		cout << a + b << '\n';
+	}
+	return 0;
+}*/
