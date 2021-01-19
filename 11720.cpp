14f96bbe6c68ee3ca9b5cc13fa974fef6d9dfcbe
Choi-Ji-Yun <64777557+J1Yun@users.noreply.github.com>
2021-01-19 15:24:29 +0900

    Sum (1)

--- 11720.cpp
+++ 11720.cpp
@@ -1,4 +1,4 @@
-/*#include <iostream>
+#include <iostream>
 using namespace std;
 int main() {
 	int n, sum=0;
@@ -11,4 +11,4 @@ int main() {
 	cout << sum << endl;
 	delete[] str;
 	return 0;
-}*/
+}
