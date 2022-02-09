83a1885485f479a285f76d0dd277ef77970852b1
J1yun <cjy5025@naver.com>
2022-02-09 20:25:04 +0900

    Problem Solving

---
+++ Python/그리디/[16953]A--B.py
@@ -0,0 +1,16 @@
+a, b = map(int, input().split())
+
+count = 0
+while b > a:
+    if b % 10 == 1:
+        b //= 10
+    elif b % 2 == 0:
+        b /= 2
+    else :
+        break
+    count += 1
+
+if a == b:
+    print(count + 1)
+else:
+    print(-1)
